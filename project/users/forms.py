f6c564f08d5da04c7acf75ab3786121b9037bc9c
heds1 <hedley.stirrat@gmail.com>
2020-04-26 10:54:33 +1200

    fix: directly customized allauth login widget to display error messages correctly

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from allauth.account.forms import LoginForm
+from django.utils.translation import gettext, gettext_lazy as _
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -14,3 +16,26 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+
+
+# add to django-allauth LoginForm (change html)
+# going off this: https://medium.com/@gavinwiener/modifying-django-allauth-forms-6eb19e77ef56
+class CustomLoginForm(LoginForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        
+        # add to all form fields
+        for fieldname, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'autofocus': '',
+                'autocapitalize': 'none',
+                'autocomplete': 'none',
+                'maxlength': '30',
+            })
+
+        # add to specific form fields
+        self.fields['login'].widget.attrs.update({
+            'placeholder': 'Email',
+            'aria-label': 'Email'
+        })
